333774a61d706b9cf6186df1fcc3921920bb2760
songkey <song960705@naver.com>
2019-12-27 22:36:50 +0900

    DP

---
+++ Dynamic Programming/BOJ_2156_포도주시식.py
@@ -0,0 +1,26 @@
+n = int(input())
+inputList = []
+
+for i in range(0, n):
+    inputList.append(int(input()))
+
+if n == 1:
+    print(inputList[0])
+elif n == 2:
+    print(inputList[0]+inputList[1])
+else:
+    dpList = [[0,0] for i in range(0,n+1)]
+    dpList[1][1] = inputList[0]
+    dpList[2][0] = dpList[1][1]
+    dpList[2][1] = inputList[0] + inputList[1]
+
+    for i in range(3, n+1):
+        max = 0
+        sum1 = sum2 = 0
+        sum1 = inputList[i-1] + inputList[i-2] + dpList[i-2][0]
+        sum2 = inputList[i-1] + dpList[i-1][0]
+        max = sum1 if sum1 > sum2 else sum2
+        dpList[i][1] = max
+        dpList[i][0] = dpList[i-1][1] if dpList[i-1][1] > dpList[i-1][0] else dpList[i-1][0]
+
+    print(dpList[n][0] if dpList[n][0] > dpList[n][1] else dpList[n][1])
--- Dynamic Programming/BOJ_9465_스티커.py
+++ Dynamic Programming/BOJ_9465_스티커.py
@@ -1,21 +1,21 @@
-T = int(input())
-for i in range(0,T):
-    N = int(input())
-    dp = [[0, 0] for i in range(0, N + 1)]
-    input1 = input()
-    input2 = input()
-    input1 = list(map(int, input1.split()))
-    input2 = list(map(int, input2.split()))
+n = int(input())
+inputList = []
 
-    dp[1][0] = input1[0]
-    dp[1][1] = input2[0]
+for i in range(0, n):
+    inputList.append(int(input()))
 
-    for j in range(2,N+1):
-        sum1 = sum2 = sum3 = sum4 = 0
-        sum1 = input1[j-1] + dp[j-1][1]
-        sum2 = input1[j-1] + dp[j-2][1]
-        dp[j][0] = sum1 if sum1 > sum2 else sum2
-        sum3 = input2[j-1] + dp[j-1][0]
-        sum4 = input2[j-1] + dp[j-2][0]
-        dp[j][1] = sum3 if sum3 > sum4 else sum4
-    print(dp[N][0] if dp[N][0] > dp[N][1] else dp[N][1])
+dpList = [[0,0] for i in range(0,n+1)]
+dpList[1][1] = inputList[0]
+dpList[2][0] = dpList[1][1]
+dpList[2][1] = inputList[0] + inputList[1]
+
+for i in range(3, n+1):
+    max = 0
+    sum1 = sum2 = 0
+    sum1 = inputList[i-1] + dpList[i-1][1]
+    sum2 = inputList[i-1] + dpList[i-1][0]
+    max = sum1 if sum1 > sum2 else sum2
+    dpList[i][1] = max
+    dpList[i][0] = dpList[i-1][0]
+
+print(dpList[n][0] if dpList[n][0] > dpList[n][1] else dpList[n][1])
